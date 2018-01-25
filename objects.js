02822652a78adc580819571eb9c9da57a81125b8
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-25 16:31:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist (playlist, artist, song) {
   return Object.assign({}, playlist, { [artist]: song});
 }
 
-function removeFromPlaylist (playlist, artist) {
+function removeFromPlaylist(playlist, artist) {
   return delete playlist.artist;
 }
