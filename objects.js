dd9440f9686c4b4cd1e43341d84c7d540aa1e907
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-25 16:39:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { beyonce: "Crazy in Love" };
 
 function updatePlaylist(obj, artist, song) {
-  return obj.artist = song;
+  obj.artist = song;
 }
 
 function removeFromPlaylist(playlist, beyonce) {
